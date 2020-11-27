54bb1e5955076ab3328ecfde5e12200aba7e7876
Kostya <KonstantinLikes@gmail.com>
2020-11-27 20:39:52 +1000

    HW2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,44 @@
-public class Main {
+import com.sun.org.apache.xpath.internal.operations.Bool;
 
+import java.sql.Time;
+import java.util.Random;
+
+public class Main {
+    private static final Boolean DEBUG = true;
     public static void main(String[] args) {
-	// write your code here
+        Random r = new Random();
+
+        MyArrayList<Integer> arr = new MyArrayList<>();
+        MyArrayList<Integer> arr2 = new MyArrayList<>();
+        MyArrayList<Integer> arr3 = new MyArrayList<>();
+        for (int i = 0; i < 100000; i++) {
+            int t = r.nextInt(100);
+            arr.add(t);
+            arr2.add(t);
+            arr3.add(t);
+        }
+
+        //System.out.println(arr);
+        double start = System.currentTimeMillis();
+        arr.bubbleSort();
+        double end = System.currentTimeMillis();
+
+        System.out.println("bubbleSort time: " + (end - start));
+
+        double start2 = System.currentTimeMillis();
+        arr2.insertionSort();
+        double end2 = System.currentTimeMillis();
+        System.out.println("insertionSort time: " + (end2 - start2));
+
+        double start3 = System.currentTimeMillis();
+        arr3.selectionSort();
+        double end3 = System.currentTimeMillis();
+        System.out.println("selectionSort time: " + (end3 - start3));
+
+        arr.selectionSort((a,b)->{return a%10 - b %10;});
+        arr.bubbleSort((a,b)->{return a%10 - b %10;});
+        arr.insertionSort((a,b)->{return a%10 - b %10;});
+
     }
+
 }
