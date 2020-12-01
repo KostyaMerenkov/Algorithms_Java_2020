f1c1c65f1d517962b878039ecca15dd22dd6bb25
Kostya <KonstantinLikes@gmail.com>
2020-12-01 12:46:04 +1000

    HW3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,36 @@
+import java.util.Stack;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(reverseWord("Привет!"));
+
+        MyDeque<Integer> myDeque = new MyDeque<>(5);
+        myDeque.insertBack(1);
+        myDeque.insertBack(3);
+        myDeque.insertBack(5);
+        System.out.println(myDeque.toString());
+        myDeque.insertFront(8);
+        myDeque.insertFront(0);
+        System.out.println(myDeque.toString());
+        myDeque.removeBack();
+        myDeque.removeFront();
+        System.out.println(myDeque.toString());
     }
+
+
+    public static String reverseWord(String str) {
+        Stack<Character> stack = new Stack<>();
+        char[] array = str.toCharArray();
+        for (int i = 0; i<array.length; i++) {
+            stack.add(array[i]);
+        }
+        String answer = "";
+        while (!stack.isEmpty()) {
+            answer = answer + stack.pop();
+        }
+        return answer;
+    }
+
+
 }
