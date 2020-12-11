0270d5af595775cf5834fb71229584debd7fc57d
Kostya <KonstantinLikes@gmail.com>
2020-12-11 17:53:50 +1000

    Загрузка файлов с урока

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,28 @@
-public class Main {
 
+import java.util.TreeMap;
+
+public class Main {
     public static void main(String[] args) {
-	// write your code here
+        MyTreeMap<Integer, String> map = new MyTreeMap<>();
+
+        map.put(5,"five");
+        map.put(1,"one");
+        map.put(2,"two");
+        map.put(3,"three");
+        map.put(4,"four");
+        map.put(2,"two two");
+
+//        System.out.println(map);
+//        System.out.println(map.get(2));
+
+//        map.deleteMin();
+        System.out.println(map);
+        System.out.println(map.size());
+
+        map.delete(5);
+        System.out.println(map);
+        System.out.println(map.size());
+
+        
     }
 }
