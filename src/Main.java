8e89ed6835bf69b97369735844614502c9cc6751
Kostya <KonstantinLikes@gmail.com>
2020-12-17 18:37:23 +1000

    HW8

--- src/Main.java
+++ src/Main.java
@@ -3,24 +3,27 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-//        ChainingHashMap<Integer, String> chm = new ChainingHashMap<>();
-//
-////        chm.put(1, "one");
-////        chm.put(2, "two");
-////        chm.put(3, "three");
-////        chm.put(4, "four");
-//
-////        System.out.println(chm.get(3));
+        ChainingHashMap<Integer, String> chm = new ChainingHashMap<>();
+
+//        chm.put(1, "one");
+//        chm.put(2, "two");
+//        chm.put(3, "three");
+//        chm.put(4, "four");
+
+//        System.out.println(chm.get(3));
         Random random = new Random();
-//
-//        for (int i = 0; i < 7 ; i++) {
-//            chm.put(random.nextInt(100),"" );
-//        }
-//
-//        System.out.println(chm);
+
+        for (int i = 0; i < 12 ; i++) {
+            chm.put(random.nextInt(10),"" );
+        }
+
+        System.out.println(chm);
+        chm.delete(0);
+        chm.delete(1);
+        System.out.println(chm);
 
 
-        LinearProbingHashMap<Integer, String> lphm = new LinearProbingHashMap<>();
+//        LinearProbingHashMap<Integer, String> lphm = new LinearProbingHashMap<>();
 //        lphm.put(1, "one");
 //        lphm.put(2, "two");
 //        lphm.put(3, "three");
@@ -28,11 +31,11 @@ public class Main {
 
         //        System.out.println(lphm.get(30));
 
-        for (int i = 0; i < 90 ; i++) {
-            lphm.put(random.nextInt(10000),"" );
-        }
-
-        System.out.println(lphm);
+//        for (int i = 0; i < 90 ; i++) {
+//            lphm.put(random.nextInt(10000),"" );
+//        }
+//
+//        System.out.println(lphm);
 
     }
 }
